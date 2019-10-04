4b307fa608be7f75ee3b0d978fe28feb3c3371d1
kousokujin <kousokujin@outlook.jp>
2019-10-04 23:37:32 +0900

    WebSocketに変更

---
+++ StaticMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ComputerResourceBroadcaster
+{
+    static class StaticMethods
+    {
+        /// <summary>
+        /// 自分のIPアドレスを取得
+        /// </summary>
+        /// <returns>IPアドレス</returns>
+        public static string[] getMyIP()
+        {
+            return getRawMyIP().Select(s => s.ToString()).ToArray();
+        }
+
+        /// <summary>
+        /// 自分のIPアドレスを取得(IPv4のみ)
+        /// </summary>
+        /// <returns></returns>
+        public static string[] getMyIPv4()
+        {
+            return getRawMyIP().Where(s => s.AddressFamily == AddressFamily.InterNetwork).Select(s => s.ToString()).ToArray();
+        }
+
+        public static IPAddress[] getRawMyIP()
+        {
+            return Dns.GetHostAddresses(Dns.GetHostName());
+        }
+    }
+}
